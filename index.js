266e90e3132d80fbd062413c93b4b4f7485fcbb6
Henry Lee <104782108+ktsnv@users.noreply.github.com>
2025-03-25 10:49:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+let menuToggled = false;
+let container = document.createElement('div');
+setTimeout(setup, 0);
+
+function setup() {
+  container.id = "lithiumContainer";
+
+  container.style.position = "fixed"; /* Makes it stay in place even when you scroll */
+  container.style.top = '0'; /* Starts at the top of the screen */
+  container.style.left = '0'; /* Starts at the left of the screen */
+  container.style.width = '100%'; /* Covers the whole width */
+  container.style.height = '100%'; /* Covers the whole height */
+  container.style.background-color = 'rgba(0, 0, 0, 0.5)'; /* Black with 50% transparency */
+  container.style.z-index = '1000'; /* Makes it appear on top of everything */
+  container.style.display = 'none'; /* Starts hidden */
+}
+
 document.addEventListener('keydown', function(event) {
-  if (event.key === '\') {
-    console.log('key pressed!');
+  if (event.key === '\\') {
+    console.log('key pressed');
+    toggle();
   }
 });
+
+function toggle() {
+  menuToggled = !menuToggled;
+  if (menuToggled) {
+    container.style.display = 'block';
+  }
+  else {
+    container.style.display = 'none';
+  }
+}
