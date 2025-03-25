cdf38de8e41cc26fedc48ae1430804b302ccaa69
Henry Lee <104782108+ktsnv@users.noreply.github.com>
2025-03-25 10:31:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 document.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-    // The Enter key was pressed!
-    console.log('Enter key pressed!');
-    // you can place any code you want to run here.
-  }
-  if (event.key === 'a') {
-    // the "a" key was pressed.
-    console.log('the "a" key was pressed!');
+  if (event.key === '\') {
+    console.log('key pressed!');
   }
 });
